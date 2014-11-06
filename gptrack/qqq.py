d332fb29da13e5223e593c6da80a34dd2c53ef55
Fabrizio Pedersoli <f.peder@gmail.com>
2014-11-05 16:50:15 -0800

    auz

--- gptrack/qqq.py
+++
@@ -1,9 +0,0 @@
-from classifier import SkinClassifier
-
-if __name__ == '__main__':
-    import sys
-    import cv2
-
-    sc = SkinClassifier()
-    sc.load('model/asd.pkl')
-    sc.predicit(cv2.imread(sys.argv[1]))
